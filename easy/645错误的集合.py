4650341ce80ba03bfb6801a37369dd3871445ce9
Bruce-zhangyuyang <bruceyuyangz@gmail.com>
2020-01-02 18:21:55 +0800

    2020-01-02优化方法

--- easy/645错误的集合.py
+++ easy/645错误的集合.py
@@ -10,17 +10,27 @@ from typing import List
 #     x.append(nums[0])
 #     return x[::-1]
 
+# def findErrorNums(nums: List[int]) -> List[int]:
+#     x = set([i for i in range(1, len(nums)+1)])
+#     nums_ = set(nums)
+#     from collections import Counter
+#     s = Counter(nums)
+#     q = list(x - nums_)
+#     for k, v in s.items():
+#         if v == 2:
+#             q.append(k)
+#             return q[::-1]
+
+# 执行用时 :240 ms, 在所有 python3 提交中击败了68.78%的用户
+# 内存消耗 :15.5 MB, 在所有 python3 提交中击败了5.21%的用户
 def findErrorNums(nums: List[int]) -> List[int]:
     x = set([i for i in range(1, len(nums)+1)])
     nums_ = set(nums)
     from collections import Counter
-    s = Counter(nums)
+    s = Counter(nums).most_common(1)
     q = list(x - nums_)
-    for k, v in s.items():
-        if v == 2:
-            q.append(k)
-            return q[::-1]
-
+    q.append(s[0][0])
+    return q
 
 
 nums = [1,2,2,4]
